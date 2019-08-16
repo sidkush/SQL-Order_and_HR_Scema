9c558a9b7b3f413f9d305e8434799ffb56f41bb0
SIDKUSH <49087799+sidkush@users.noreply.github.com>
2019-08-16 14:40:49 +0530

    Add files via upload

---
+++ Lab Exercise 01.sql
@@ -0,0 +1,29 @@
+#Q.no(01).Show product id, description and total worth of each product.
+
+
+#Q.no(02).Show order details which are yet to be shipped.
+
+
+#Q.no(03).Display the details of products where the product category is any of 2050, 2053 or 2055.
+
+
+#Q.no(04).List the details of customers whose creation date is before ’12-Jan-2006’ and email address contains ‘gmail’ or ‘yahoo’ and user name starts with ‘dave’.
+
+
+#Q.no(05).Display details of customer who have Gmail account, as below:
+          #<Customer full name> (<customer user name>) created on <date>. Contact Phone: <Phone no.> E-mail: <E-mail id>
+          
+          
+#Q.no(06).Display the product Id, product description and total revenue for each product, if all the products are considered sold.
+
+
+#Q.no(07).List the details of products with class code 2050 where price is in the range of 10000 and 30000 and available quantity is more than 15.
+
+
+#Q.no(08).Display the distinct id’s of the products which have been sold.
+
+
+#Q.no(09).Show order id, order_date and shipment dates of all shipped orders in an ascending order of customer ids and descending order of order dates.
+
+
+#Q.no(10).Show order id, order_date and shipment dates of all orders. For those orders which are yet to be shipped, show shipment date as 3 days from order date. (Use NULL related function).
---
+++ Lab Exercise 03.sql
@@ -0,0 +1,30 @@
+
+#Q.no(1).Write a query to perform join on the Employees and Department tables and display only employees who have a manager ID of 149.
+
+
+#Q.no(2).list other products that were bought along with product id 212 it provided their total qty bought is > 5.
+
+
+#Q.no(3).Which are the most and least sold products (quantity-wise)? Show their id and description.
+
+
+#Q.no(4).List customers who have not purchased any product. (Use a subquery).
+
+
+#Q.no(5).For a given product id =222,list which other products were also bought along with it. (Use Exists).
+
+
+#Q.no(6).List the most expensive products in their respective classes.
+
+
+#Q.no(7).Assume all items of an order are packed into one single carton (box). 
+        #Write a query to identify the optimum carton (carton with the least volume whose volume is greater than the total volume of all items)for a given order.
+
+
+#Q.no(8).Which shipper has shipped highest volume of items? (Hint: Each product has L x W x H dimensions).
+
+
+
+
+
+
